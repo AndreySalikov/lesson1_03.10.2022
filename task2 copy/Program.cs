fcb626b8b875943833e7cfeebaa01fadd24c450a
Andrey Salikov <79645878868@ya.ru>
2022-10-03 19:15:26 +0300

    Added new solution Task2_1

--- task2 copy/Program.cs
+++ task2 copy/Program.cs
@@ -4,12 +4,30 @@
 
 Console.WriteLine("Введите номер дня недели: ");
 int day = Convert.ToInt32(Console.ReadLine() ?? "0");
-string [] days = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
-if(day < 1 || day > 7)
+// string [] days = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
+// if(day < 1 || day > 7)
+// {
+//     Console.WriteLine("Такого дня недели не существует!");
+// }
+// else
+// {
+//     Console.WriteLine(days [day -1]);
+// }
+
+switch (day)
 {
-    Console.WriteLine("Такого дня недели не существует!");
-}
-else
-{
-    Console.WriteLine(days [day -1]);
+    case 1: Console.WriteLine("Понедельник");
+    break;
+    case 2: Console.WriteLine("Вторник");
+    break;
+    case 3: Console.WriteLine("Среда");
+    break;
+    case 4: Console.WriteLine("Четверг");
+    break;
+    case 5: Console.WriteLine("Пятница");
+    break;
+    case 6: Console.WriteLine("Суббота");
+    break;
+    case 7: Console.WriteLine("Воскресенье");
+    break;
 }
