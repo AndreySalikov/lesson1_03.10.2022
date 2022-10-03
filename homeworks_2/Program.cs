34915964d6cfe627c5c2606571d18507a8c68fb7
Andrey Salikov <79645878868@ya.ru>
2022-10-03 21:33:32 +0300

    Added a solution for homeworks

--- homeworks_2/Program.cs
+++ homeworks_2/Program.cs
@@ -4,3 +4,24 @@
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+System.Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if(a > b || a == b)
+{
+    max = a;
+}
+else 
+{
+    max = b;
+}
+if(c > max || c == max) 
+{
+    max = c;
+}
+    System.Console.WriteLine($" Вы ввели {a}, {b}, {c} - Максимальное из них = {max}");
---
+++ homeworks_2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+string a;
+int[] b = new int[100];
+int i=0;
+while ((a = Console.ReadLine()) != "end") //Получаем строку и проверяем ее содержание
+    b[i++] = Convert.ToInt32(a); //в стркое нет "конца" - значит конвертим и пишем в массив
+
+for (int k = 0; k < i; k++) //вывод заполненных ячеек массива
+    Console.WriteLine(b[k]);
+
+Console.ReadKey(true);
