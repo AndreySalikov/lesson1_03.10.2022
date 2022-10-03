80cc97dadbe61a8d8457b2da0fa8be6c098eb300
Andrey Salikov <79645878868@ya.ru>
2022-10-03 17:35:20 +0300

    Added a new solution for the first example

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите первое число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+if(a == b * b)
+{
+    Console.WriteLine($"Число {a} являеться квадратом {b}");
+}
+else if(b == a * a)
+{
+    Console.WriteLine($"Число {b} являеться квадратом {a}");
+}
+else
+{
+    Console.WriteLine($"Числа не являються квадратами друг друга");
+}
