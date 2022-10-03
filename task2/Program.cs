0ca079d4c17f18a5948a6b8ee5189a3f6e1c24b3
Andrey Salikov <79645878868@ya.ru>
2022-10-03 17:57:59 +0300

    Added new solution Task2_1

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,19 +2,37 @@
 // 3 -> Среда 
 // 5 -> Пятница
 
-Console.WriteLine("Введите первое число: ");
+Console.WriteLine("Введите номер дня недели: ");
 int a = Convert.ToInt32(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите первое число: ");
-int b = int.Parse(Console.ReadLine() ?? "0");
-if(a == b * b)
+if(a == 1)
 {
-    Console.WriteLine("Число "+a+ "являеться квадратом" +b+".");
+    Console.WriteLine("Понедельник");
 }
-else if(b == a * a)
+else if(a == 2)
 {
-    Console.WriteLine($"Число {b} являеться квадратом {a}.");
+    Console.WriteLine("Вторник");
+}
+else if(a == 3)
+{
+    Console.WriteLine($"Среда");
+}
+else if(a == 4)
+{
+    Console.WriteLine($"Четверг");
+}
+else if(a == 5)
+{
+    Console.WriteLine($"Пятьница");
+}
+else if(a == 6)
+{
+    Console.WriteLine($"Суббота");
+}
+else if(a == 7)
+{
+    Console.WriteLine($"Воскресенье");
 }
 else
 {
-    Console.WriteLine($"Числа не являються квадратами друг друга");
+    Console.WriteLine($"Такого дня недели не существует");
 }
