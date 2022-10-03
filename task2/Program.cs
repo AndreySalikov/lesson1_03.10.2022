663305da3d915b9ac7e2f6d5386d053606ab6485
Andrey Salikov <79645878868@ya.ru>
2022-10-03 17:40:42 +0300

    Added a new solution for the first example

--- task2/Program.cs
+++ task2/Program.cs
@@ -8,11 +8,11 @@ Console.WriteLine("Введите первое число: ");
 int b = int.Parse(Console.ReadLine() ?? "0");
 if(a == b * b)
 {
-    Console.WriteLine($"Число {a} являеться квадратом {b}");
+    Console.WriteLine("Число "+a+ "являеться квадратом" +b+".");
 }
 else if(b == a * a)
 {
-    Console.WriteLine($"Число {b} являеться квадратом {a}");
+    Console.WriteLine($"Число {b} являеться квадратом {a}.");
 }
 else
 {
