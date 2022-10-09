1fc5841fdca054046d4bdea6030cfffacb0009c8
Andrey Salikov <79645878868@ya.ru>
2022-10-10 01:48:02 +0300

    Rename folder

--- task2_1/Program.cs
+++ task2_1/Program.cs
@@ -3,13 +3,36 @@
 // 5 -> Пятница
 
 Console.WriteLine("Введите номер дня недели: ");
-int day = Convert.ToInt32(Console.ReadLine() ?? "0");
-string [] days = {"Понедельник","Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье"};
-if(day < 1 || day > 7)
+int a = Convert.ToInt32(Console.ReadLine() ?? "0");
+if(a == 1)
 {
-    Console.WriteLine("Такого дня недели не существует!");
+    Console.WriteLine("Понедельник");
+}
+else if(a == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if(a == 3)
+{
+    Console.WriteLine($"Среда");
+}
+else if(a == 4)
+{
+    Console.WriteLine($"Четверг");
+}
+else if(a == 5)
+{
+    Console.WriteLine($"Пятьница");
+}
+else if(a == 6)
+{
+    Console.WriteLine($"Суббота");
+}
+else if(a == 7)
+{
+    Console.WriteLine($"Воскресенье");
 }
 else
 {
-    Console.WriteLine(days [day -1]);
+    Console.WriteLine($"Такого дня недели не существует");
 }
