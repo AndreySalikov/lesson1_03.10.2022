0e066026bf59cf6bbbeb71d718628a16870e6a35
Andrey Salikov <79645878868@ya.ru>
2022-10-14 19:28:13 +0300

    new folder added

--- task17/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// Lesson 3
-// See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
-// Задача 6*: Напишите программу, которая генерирует последовательность случайных чисел до тех пор, 
-// пока не сгенерирует кратное число введенному в начале.
