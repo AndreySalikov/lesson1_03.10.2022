297b2667413fab88f72048512972c8646b70dcb4
Andrey Salikov <79645878868@ya.ru>
2022-10-22 13:12:20 +0300

    Added new folder structure

---
+++ Tasks/Part_6/Task_5/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Задача 4: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
